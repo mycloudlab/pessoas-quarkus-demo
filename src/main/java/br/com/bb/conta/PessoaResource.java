cce230dede67977071d242f8601ce21aa9bf9ddd
Carlos Alberto (Euprogramador) <euprogramador@gmail.com>
2021-08-26 16:18:50 -0300

    Update PessoaResource.java

--- src/main/java/br/com/bb/conta/PessoaResource.java
+++ src/main/java/br/com/bb/conta/PessoaResource.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,6 +17,7 @@ import javax.ws.rs.core.Response;
 public class PessoaResource {
 
   Set<Pessoa> pessoas = new HashSet<>();
+  private long delay;
 
   public PessoaResource(){
     pessoas.add(new Pessoa("00045789100", "CARLOS ALBERTO RODRIGUES DE OLIVEIRA SANTANA", "QUADRA 3 CASA 27", "SAO JOSE", "SAO SEBASTIAO", "DF", "71693004", "", "", ""));
@@ -27,12 +29,28 @@ public class PessoaResource {
     System.out.println("Recebendo request");
     return pessoas;
   }
+  
+  @GET
+  @Path("/delay")
+  @Produces(MediaType.APPLICATION_JSON)
+  public void delay(@QueryParam("delay") long delay) {
+    System.out.println("delay configurado:" + delay);
+    this.delay = delay;
+  }
+
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/{cpf}")
   public Response retrive(@PathParam("cpf") String cpf) {
     System.out.println("Recebendo request");
+    try {
+      System.out.println("aguardando..." + delay);
+      Thread.currentThread().sleep(delay);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     List<Pessoa> pessoas = this.pessoas.stream().filter((p)->cpf.equals(p.getCpf()))  .collect(Collectors
         .toCollection(ArrayList::new));
     
